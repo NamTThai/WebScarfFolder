8e7bc717f89d051fa4a8441f4fbf22cc84c2bddc
Nam Thai <nthai@ciosolutions.com>
2015-08-10 11:09:21 -0700

    Finish with routing

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,21 @@ var router = express.Router();
 
 // Get home page
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    route: "one"
+  });
 });
 
 router.get('/one', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    route: "one"
+  });
 });
 
 router.get('/two', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    route: "two"
+  });
 });
 
 module.exports = router;
