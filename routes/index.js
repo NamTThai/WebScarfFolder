c84087157a82346bf959b2f2b0f53b9c2379e93a
NamTThai <nam.thai.cs@gmail.com>
2015-08-09 22:46:01 -0700

    routing basic

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ var express = require('express');
 var router = express.Router();
 
 // Get home page
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/one', function(req, res) {
+  res.render('index');
+});
+
+router.get('/two', function(req, res) {
   res.render('index');
 });
 
